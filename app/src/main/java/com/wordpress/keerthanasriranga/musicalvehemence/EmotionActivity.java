8beacbe87d81a3279f5ecd28517635987380da6f
Keerthana Sriranga <keerthanasriranga11@gmail.com>
2017-04-14 19:16:11 +0530

    Added random images and play/pause emobutton

--- app/src/main/java/com/wordpress/keerthanasriranga/musicalvehemence/EmotionActivity.java
+++ app/src/main/java/com/wordpress/keerthanasriranga/musicalvehemence/EmotionActivity.java
@@ -3,12 +3,15 @@ package com.wordpress.keerthanasriranga.musicalvehemence;
 import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.microsoft.projectoxford.emotion.EmotionServiceClient;
 import com.microsoft.projectoxford.emotion.EmotionServiceRestClient;
@@ -21,18 +24,28 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class EmotionActivity extends AppCompatActivity {
 
     public EmotionServiceClient emotionServiceClient = new EmotionServiceRestClient("a421fd6dc6a24147abe0942f52e270ca");
+    TextView emotion;
+    MediaPlayer mplayer;
+    Button emoplay;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emotion);
+        emotion = (TextView) findViewById(R.id.emotion);
+        emoplay = (Button) findViewById(R.id.emoplay);
+        int[] p = {R.drawable.angry, R.drawable.disgust,R.drawable.happy, R.drawable.sad, R.drawable.surprise};
 
-        final Bitmap mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.happy);
+        Random r = new Random();
+        int i1 = 0 +  r.nextInt(4);
+
+        final Bitmap mBitmap = BitmapFactory.decodeResource(getResources(), p[i1]);
         final ImageView imageView = (ImageView)findViewById(R.id.imageView);
         imageView.setImageBitmap(mBitmap);
 
@@ -90,6 +103,25 @@ public class EmotionActivity extends AppCompatActivity {
                 emotionTask.execute(inputStream);
             }
         });
+        emoplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mplayer!=null) {
+                    if (mplayer.isPlaying()) {
+                        mplayer.pause();
+                        emoplay.setText("Play");
+                    } else {
+                        mplayer.start();
+                        emoplay.setText("Pause");
+                    }
+                }
+                else
+                {
+                    Toast.makeText(getApplication(),
+                            "Recognise the emotion first", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     private String getEmo(RecognizeResult res) {
@@ -108,22 +140,61 @@ public class EmotionActivity extends AppCompatActivity {
 
         Collections.sort(list);
         double maxNum = list.get(list.size()-1);
-        if(maxNum == scores.anger)
-            return  "Anger";
-        else if(maxNum == scores.happiness)
-            return  "happiness";
-        else if(maxNum == scores.contempt)
-            return  "contempt";
-        else if(maxNum == scores.disgust)
-            return  "disgust";
-        else if(maxNum == scores.fear)
-            return  "fear";
-        else if(maxNum == scores.fear)
-            return  "neutral";
-        else if(maxNum == scores.sadness)
-            return  "sadness";
-        else if(maxNum == scores.surprise)
-            return  "surprise";
-        else return "Neutral";
+        if(maxNum == scores.anger) {
+            emotion.setText("Anger");
+            mplayer = mplayer.create(EmotionActivity.this, R.raw.pakkamvathu);
+            mplayer.start();
+            return "Anger";
+        }
+        else if(maxNum == scores.happiness) {
+            emotion.setText("Happy");
+            mplayer = mplayer.create(EmotionActivity.this, R.raw.ifeelgood);
+            mplayer.start();
+            return "happiness";
+        }
+        else if(maxNum == scores.contempt) {
+            emotion.setText("contempt");
+            mplayer = mplayer.create(EmotionActivity.this, R.raw.perfecttwo);
+            mplayer.start();
+            return "contempt";
+        }
+        else if(maxNum == scores.disgust) {
+            emotion.setText("disgust");
+            mplayer = mplayer.create(EmotionActivity.this, R.raw.haledil);
+            mplayer.start();
+            return "disgust";
+        }
+        else if(maxNum == scores.fear) {
+            emotion.setText("fear");
+            mplayer = mplayer.create(EmotionActivity.this, R.raw.zehnaseeb);
+            mplayer.start();
+            return "fear";
+        }
+        else if(maxNum == scores.neutral) {
+            emotion.setText("neutral");
+            mplayer = mplayer.create(EmotionActivity.this, R.raw.first);
+            mplayer.start();
+            return "neutral";
+        }
+        else if(maxNum == scores.sadness) {
+            emotion.setText("sad");
+            mplayer = mplayer.create(EmotionActivity.this, R.raw.neeyaaro);
+            mplayer.start();
+            return "sadness";
+        }
+        else if(maxNum == scores.surprise) {
+            emotion.setText("surprise");
+            mplayer = mplayer.create(EmotionActivity.this, R.raw.iskiuski);
+            mplayer.start();
+            return "surprise";
+        }
+        else{
+            emotion.setText("neutral");
+            mplayer = mplayer.create(EmotionActivity.this, R.raw.ifeelgood);
+            mplayer.start();
+            return "Neutral";
+        }
     }
+
+
 }
