ebc4bccf6e18cdd58b77a91261eaf3db319071e6
Keerthana Sriranga <keerthanasriranga11@gmail.com>
2017-04-14 13:35:56 +0530

    Added play pause button and changed layout

--- app/src/main/java/com/wordpress/keerthanasriranga/musicalvehemence/MainActivity.java
+++ app/src/main/java/com/wordpress/keerthanasriranga/musicalvehemence/MainActivity.java
@@ -1,17 +1,25 @@
 package com.wordpress.keerthanasriranga.musicalvehemence;
 
 import android.media.MediaPlayer;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SeekBar;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+
+import android.os.Handler;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,39 +30,131 @@ public class MainActivity extends AppCompatActivity {
     ListAdapter adapter;
 
     MediaPlayer mediaPlayer;
+    Handler handler;
+    SeekBar seekBar;
+    Runnable runnable;
+    Button pausebutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        listView = (ListView)findViewById(R.id.listView);
-
         list = new ArrayList<>();
+        handler = new Handler();
+
+        listView = (ListView) findViewById(R.id.listView);
+        setListViewHeightBasedOnChildren(listView);
+
+        pausebutton = (Button) findViewById(R.id.pausebutton);
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+
 
         Field[] fields = R.raw.class.getFields();
-        for(int i=0; i< fields.length; i++){
+        for (int i = 0; i < fields.length; i++) {
             list.add(fields[i].getName());
         }
 
         list.remove(0);
-        list.remove(fields.length-2);
+        list.remove(fields.length - 2);
 
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
         listView.setAdapter(adapter);
 
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view,int i, long l){
-
-                if(mediaPlayer!=null) {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> adapterView, View view,int i,long l){
+                if (mediaPlayer!=null){
                     mediaPlayer.release();
                 }
-                int resID = getResources().getIdentifier(list.get(i), "raw", getPackageName());
-                mediaPlayer = MediaPlayer.create(MainActivity.this , resID);
+                int resId = getResources().getIdentifier(list.get(i),"raw",getPackageName());
+                mediaPlayer = mediaPlayer.create(MainActivity.this,resId);
+
+                playcycle();
                 mediaPlayer.start();
+                seekBar.setMax(mediaPlayer.getDuration());
+
             }
         });
+
+
+
+
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser)
+                {
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        pausebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    pausebutton.setText("Play");
+                } else {
+                    mediaPlayer.start();
+                    pausebutton.setText("Pause");
+                }
+            }
+        });
+
+    }
+    public void playcycle()
+    {
+        seekBar.setProgress(mediaPlayer.getCurrentPosition());
+
+        if(mediaPlayer.isPlaying())
+        {
+            runnable=new Runnable() {
+                @Override
+                public void run() {
+                    playcycle();
+                }
+            };
+            handler.postDelayed(runnable,1000);
+        }
+    }
+
+
+
+
+
+
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null)
+            return;
+
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.UNSPECIFIED);
+        int totalHeight = 0;
+        View view = null;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            view = listAdapter.getView(i, view, listView);
+            if (i == 0)
+                view.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, ActionBar.LayoutParams.WRAP_CONTENT));
+
+            view.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += view.getMeasuredHeight();
+        }
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
     }
 }
