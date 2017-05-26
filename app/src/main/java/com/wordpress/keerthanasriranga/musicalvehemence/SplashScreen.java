4a5d35ed2e73350442f3ef3fe465be8cc0e1d114
Keerthana Sriranga <keerthanasriranga11@gmail.com>
2017-05-26 09:57:03 +0530

    Removed Title Bar

--- app/src/main/java/com/wordpress/keerthanasriranga/musicalvehemence/SplashScreen.java
+++ app/src/main/java/com/wordpress/keerthanasriranga/musicalvehemence/SplashScreen.java
@@ -6,6 +6,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -14,7 +15,9 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 
         setContentView(R.layout.activity_splash_screen);
